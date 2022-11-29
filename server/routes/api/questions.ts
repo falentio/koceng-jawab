a3e53b59901f4217a6f13ce56d3c5362ec53ab31
Kevin Falentio <riv.kevinfalent.io@gmail.com>
2022-11-30 06:48:39 +0700

    Update questions.ts

--- server/routes/api/questions.ts
+++ server/routes/api/questions.ts
@@ -20,12 +20,12 @@ export const subjects = {
 }
 
 const defaultUserAgent = () => `Mozilla/5.${Math.random() * 3 | 0} (X11; Linux x86_64) AppleWebKit/${537 + Math.random() * 10 | 0}.36 (KHTML, like Gecko) Chrome/${91 - Math.random() * 4 | 0}.${Math.random() * 4 | 0}.${4472 - Math.random() * 100 | 0}.${124 + Math.random() * 40 | 0} Safari/${537 - Math.random() * 20 | 0}.${36 + Math.random() * 30}`
-const endpoint = () => `https://script.google.com/macros/s/AKfycby1LQUD6BfG4lDkTgZgbnkCJXgyBHe8-h3JwCQvLoIb3YHeRv7MuIBmOpeIxi4z4muHNw/callback?nocache_id=${Math.random() * 500 | 0}`
+const endpoint = () => `https://script.google.com/macros/s/AKfycby1LQUD6BfG4lDkTgZgbnkCJXgyBHe8-h3JwCQvLoIb3YHeRv7MuIBmOpeIxi4z4muHNw/callback?nocache_id=${Math.random() * 5000 | 0}`
 
 async function getQuestions(subject: keyof typeof subjects, student: number, userAgent = defaultUserAgent()) {
   const body = new URLSearchParams()
   body.set("request", JSON.stringify(["ambilMapel",`[${student},${subjects[subject]}]`,null,[0],null,null,true,0]))
-  const req = new Request("https://script.google.com/macros/s/AKfycby1LQUD6BfG4lDkTgZgbnkCJXgyBHe8-h3JwCQvLoIb3YHeRv7MuIBmOpeIxi4z4muHNw/callback?nocache_id=5", {
+  const req = new Request(endpoint(), {
     "headers": {
       "accept": "*/*",
       "accept-language": "en-US,en;q=0.9",
@@ -36,7 +36,7 @@ async function getQuestions(subject: keyof typeof subjects, student: number, use
       "sec-gpc": "1",
       "x-same-domain": "1",
       "user-agent": userAgent,
-      "cookie": `1P_JAR=${new Date().toISOString().slice(0, 10)}-1; AEC=AakniGP_${Math.random().toString(36).slice(3)}A4tDEiztUvpxei-pOjAW0QgSvOXG_RAXAuceRXnqw; NID=511=rrly3ll_${Math.random().toString(36).slice(3)}NRE20L4aoR6Q7LbceTvB7c6EaHYiq1czHDadO5pdKTtLwd4_${Math.random().toString(36).slice(3)}MT9a8wgsdfoDBz9T3QoUOJqG_o2bF_e7bB9_unozu3jT-${Math.random().toString(36).slice(3)}${Math.random().toString(36).slice(3)}`,
+      //"cookie": `1P_JAR=${new Date().toISOString().slice(0, 10)}-1; AEC=AakniGP_${Math.random().toString(36).slice(3)}A4tDEiztUvpxei-pOjAW0QgSvOXG_RAXAuceRXnqw; NID=511=rrly3ll_${Math.random().toString(36).slice(3)}NRE20L4aoR6Q7LbceTvB7c6EaHYiq1czHDadO5pdKTtLwd4_${Math.random().toString(36).slice(3)}MT9a8wgsdfoDBz9T3QoUOJqG_o2bF_e7bB9_unozu3jT-${Math.random().toString(36).slice(3)}${Math.random().toString(36).slice(3)}`,
     },
     "referrer": "https://script.google.com/macros/s/AKfycby1LQUD6BfG4lDkTgZgbnkCJXgyBHe8-h3JwCQvLoIb3YHeRv7MuIBmOpeIxi4z4muHNw/exec",
     "referrerPolicy": "strict-origin-when-cross-origin",
