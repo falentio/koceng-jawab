84253569abffeaf1fd20e5e4fd07711d43437a70
falentio <riv.kevinfalent.io@gmail.com>
2023-03-08 19:43:57 +0700

    hhhhh

--- fe/src/routes/+page.svelte
+++ fe/src/routes/+page.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col space-y-2 mx-auto mt-4">
-	{#each data.pas.map(s => subject).sort() as subject }
+	{#each data.pas.map(s => s.subject).sort() as subject }
 		<a href="/{subject}" class="bg-blue-200 p-2 rounded"> {subject} </a>
 	{/each}
 </div>
